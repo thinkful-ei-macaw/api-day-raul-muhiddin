6f907a57b201f0643ccf32ed12c80361855727a5
Raul Garcia <punkvomit21@gmail.com>
2020-02-21 17:57:23 -0500

    step 10

--- src/api.js
+++ src/api.js
@@ -15,17 +15,15 @@ function createItem(name) {
   });
 }
 
-function updateItem(id, updateData) {
-  return fetch(`${BASE_URL}/items/${id}`, {
-    method: 'PATCH',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(updateData)
+const deleteItem = function (id) {
+  return fetch(BASE_URL + '/items/' + id, {
+    method: 'DELETE'
   });
-}
+};
 
 export default {
   getItems,
   createItem,
-  updateItem
+  deleteItem
 };
 
