b2e8e8457131c53d5562dfaa6f66314de4ab11bb
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-02-21 11:40:47 -0800

    step 7

--- src/api.js
+++ src/api.js
@@ -15,8 +15,17 @@ function createItem(name) {
   });
 }
 
+function updateItem(id, updateData) {
+  return fetch(`${BASE_URL}/items/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updateData)
+  });
+}
+
 export default {
   getItems,
   createItem,
+  updateItem
 };
 
